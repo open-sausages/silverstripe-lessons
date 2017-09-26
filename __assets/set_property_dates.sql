6fc08b7f79013b107702eb2a7252681708fb6428
Aaron Carlino <unclecheese@leftandmain.com>
2017-09-26 13:49:03 +1300

    Lesson 15 complete

--- __assets/set_property_dates.sql
+++
@@ -1,6 +0,0 @@
-UPDATE SilverStripe_Lessons_Property SET AvailableStart = FROM_UNIXTIME(
-        UNIX_TIMESTAMP(NOW()) + FLOOR(0 + (RAND() * 31536000))
-);
-UPDATE SilverStripe_Lessons_Property SET AvailableEnd = FROM_UNIXTIME(
-        UNIX_TIMESTAMP(AvailableStart) + FLOOR(1 + (RAND() * 1209600))
-);
